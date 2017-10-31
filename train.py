bb0e270c73dc89c616c17d01328e5182ffe997f2
Xingdi (Eric) Yuan <eric.yuan@maluuba.com>
2017-10-30 21:29:56 -0400

    minor

--- train.py
+++ train.py
@@ -155,14 +155,13 @@ def the_main_function(config_dir='config', update_dict=None):
                         else:
                             logger.info('learning rate %.5f reached lower bound' % (learning_rate))
                     be_patient += 1
-            logger.info("========================================================================\n")
 
             test_f1, test_em, test_nll_loss = evaluate(model=_model, data=test_data, criterion=criterion,
                                                        trim_function=squad_trim, char_level_func=add_char_level_stuff,
                                                        word_id2word=word_vocab, char_word2id=char_word2id,
                                                        batch_size=valid_batch_size, enable_cuda=model_config['scheduling']['enable_cuda'])
-            logger.info("------------------------------------------------------------------------------------\n")
             logger.info("test: nll loss=%.5f, f1=%.5f, em=%.5f" % (test_nll_loss, test_f1, test_em))
+            logger.info("========================================================================\n")
 
     # At any point you can hit Ctrl + C to break out of training early.
     except KeyboardInterrupt:
